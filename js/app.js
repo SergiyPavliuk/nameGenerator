493318aacc6bfc01458d7de473af958b5b69e8a2
SergiyPavliuk <sergiy_pavliuk@ukr.net>
2018-07-16 19:14:19 +0300

    Ajax has been changed to Fetch Api

--- js/app.js
+++ js/app.js
@@ -9,42 +9,36 @@ function loadNames(e) {
     const amount = document.getElementById('quantity').value;
 
     // Build the URL
-     let url = 'http://uinames.com/api/?';
-     // Read the origin and append to the url
-     if(origin !== ''){
-          url += `region=${origin}&`;
-     }
-     // Read the genre and append to the url
-     if(genre !== ''){
-          url += `gender=${genre}&`;
-     }    
-      // Read the amount and append to the url
-     if(amount !== ''){
-          url += `amount=${amount}&`;
-     }
-
-    //AJAX call
-    const xhr = new XMLHttpRequest();
-
-    //open the connection
-    xhr.open('GET', url, true);
-
-    //execude the function
-    xhr.onload = function () {
-        if (this.status === 200) {
-            const names = JSON.parse(this.responseText);
-            //insert into html
-            let html = '<h2>Згенеровані імена:</h2>';
+    let url = 'http://uinames.com/api/?';
+    // Read the origin and append to the url
+    if (origin !== '') {
+        url += `region=${origin}&`;
+    }
+    // Read the genre and append to the url
+    if (genre !== '') {
+        url += `gender=${genre}&`;
+    }
+    // Read the amount and append to the url
+    if (amount !== '') {
+        url += `amount=${amount}&`;
+    }
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (names) {
+            let html = '';
+            html = '<h2>Згенеровані імена:</h2>';
             html += '<ul class="list">';
             names.forEach(name => {
                 html += `
                     <li>${name.name}</li>
                 `;
             });
-            html +='</ul>';
-
-            document.querySelector('#result').innerHTML = html;
-        }
-    }
-    xhr.send();
+            html += '</ul>';
+            document.getElementById('result').innerHTML = html;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 }
